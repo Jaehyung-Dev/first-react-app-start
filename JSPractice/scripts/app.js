9b340f8f574fdfabfe74910d73232823d26450d9
dev2024dev <publicdev2024@gmail.com>
2024-12-01 14:45:27 +0900

    Add JSPractice

---
+++ JSPractice/scripts/app.js
@@ -0,0 +1,12 @@
+// import { apiKey, abc as a } from './util.js';
+// import apiKey2 from "./util.js";
+
+import * as util from "./util.js";
+
+// console.log(apiKey);
+// console.log(apiKey2);
+// console.log(a);
+
+console.log(util.apiKey);
+console.log(util.default);
+console.log(util.abc);
---
+++ JSPractice/scripts/util.js
@@ -0,0 +1,4 @@
+export let apiKey = "123456789";
+export let abc = "abc";
+
+export default "987654321";
